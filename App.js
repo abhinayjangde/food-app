022d67b436fab2695811e872493ee1509d121830
Abhinay Jangde <abhinayjangde@gmail.com>
2025-01-17 23:21:15 +0530

    header components is built

--- App.js
+++ App.js
@@ -1,6 +1,35 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
 
-const heading = React.createElement("h1", { id: "heading" }, "Hello from React Parcel")
+// const heading = React.createElement("h1", { id: "heading" }, "Hello from React Parcel")
+
+// Header Component
+const Header = ()=>{
+    return (
+        <div className="header">
+            <div className="logo-container">
+                <img className="logo" src="https://png.pngtree.com/png-vector/20220705/ourmid/pngtree-food-logo-png-image_5687686.png" />
+            </div>
+            <div className="nav-items" >
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                    <li>Cart</li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+// RootLayout
+const App = ()=>{
+    return (
+        <div className="app">
+            <Header/>
+        </div>
+    )
+}
+
 const root = ReactDOM.createRoot(document.getElementById("root"))
-root.render(heading)
+root.render(<App/>)
