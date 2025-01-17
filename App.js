7ea72f112394c6121415471aa4c18372fde3355b
Abhinay Jangde <abhinayjangde@gmail.com>
2025-01-17 21:28:39 +0530

    dynamic food name is rendered

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
-import React from "react"
+import React, { useState } from "react"
 import ReactDOM from "react-dom/client"
 
 // const heading = React.createElement("h1", { id: "heading" }, "Hello from React Parcel")
 
 // Header Component
-const Header = ()=>{
+const Header = () => {
     return (
         <div className="header">
             <div className="logo-container">
@@ -22,53 +22,105 @@ const Header = ()=>{
     )
 }
 
+const foodList = [
+    {
+        "strMeal": "Baingan Bharta",
+        "strMealThumb": "https://www.themealdb.com/images/media/meals/urtpqw1487341253.jpg",
+        "idMeal": "52807"
+    },
+    {
+        "strMeal": "Bread omelette",
+        "strMealThumb": "https://www.themealdb.com/images/media/meals/hqaejl1695738653.jpg",
+        "idMeal": "53076"
+    },
+    {
+        "strMeal": "Chicken Handi",
+        "strMealThumb": "https://www.themealdb.com/images/media/meals/wyxwsp1486979827.jpg",
+        "idMeal": "52795"
+    },
+    {
+        "strMeal": "Dal fry",
+        "strMealThumb": "https://www.themealdb.com/images/media/meals/wuxrtu1483564410.jpg",
+        "idMeal": "52785"
+    },
+    {
+        "strMeal": "Kidney Bean Curry",
+        "strMealThumb": "https://www.themealdb.com/images/media/meals/sywrsu1511463066.jpg",
+        "idMeal": "52868"
+    },
+    {
+        "strMeal": "Lamb Biryani",
+        "strMealThumb": "https://www.themealdb.com/images/media/meals/xrttsx1487339558.jpg",
+        "idMeal": "52805"
+    },
+    {
+        "strMeal": "Lamb Rogan josh",
+        "strMealThumb": "https://www.themealdb.com/images/media/meals/vvstvq1487342592.jpg",
+        "idMeal": "52808"
+    },
+    {
+        "strMeal": "Matar Paneer",
+        "strMealThumb": "https://www.themealdb.com/images/media/meals/xxpqsy1511452222.jpg",
+        "idMeal": "52865"
+    },
+    {
+        "strMeal": "Nutty Chicken Curry",
+        "strMealThumb": "https://www.themealdb.com/images/media/meals/yxsurp1511304301.jpg",
+        "idMeal": "52851"
+    },
+    {
+        "strMeal": "Recheado Masala Fish",
+        "strMealThumb": "https://www.themealdb.com/images/media/meals/uwxusv1487344500.jpg",
+        "idMeal": "52809"
+    },
+    {
+        "strMeal": "Smoked Haddock Kedgeree",
+        "strMealThumb": "https://www.themealdb.com/images/media/meals/1550441275.jpg",
+        "idMeal": "52964"
+    },
+    {
+        "strMeal": "Tandoori chicken",
+        "strMealThumb": "https://www.themealdb.com/images/media/meals/qptpvt1487339892.jpg",
+        "idMeal": "52806"
+    }
+]
 // RestaurantCard
-const RestaurantCard = ()=>{
+const RestaurantCard = ({ foodList }) => {
+    console.log(foodList)
     return (
         <div className="res-card">
-            <img className="food-img" alt="pizza" src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/FOOD_CATALOG/IMAGES/CMS/2024/12/18/35139b49-285b-4b15-ae85-94c910ff612c_69cb0003-937e-49d0-a21a-5b46ec2dd500.jpg_compressed" />
-            <h3>The Wood Fired Pizzeria</h3>
-            <h5>4.4 (695 ratings)</h5>
-            <h5>₹300 for two</h5>
+            <img className="food-img" alt="pizza" src={foodList.strMealThumb} />
+            <h3>{foodList.strMeal}</h3>
+            <h5>{foodList.idMeal}</h5>
         </div>
     )
 }
+
+
 // Body
-const Body = ()=>{
+const Body = () => {
     return (
         <div className="body" >
-            <div className="search">Search Bar</div>
+            <div className="search">Search</div>
             <div className="res-container" >
-                {/* TODO : Restaurant Cart   */}
-                <RestaurantCard/>
-                <RestaurantCard/>
-                <RestaurantCard/>
-                <RestaurantCard/>
-                <RestaurantCard/>
-                <RestaurantCard/>
-                <RestaurantCard/>
-                <RestaurantCard/>
-                <RestaurantCard/>
-                <RestaurantCard/>
-                <RestaurantCard/>
-                <RestaurantCard/>
-                <RestaurantCard/>
-                <RestaurantCard/>
-                <RestaurantCard/>
-                <RestaurantCard/>
+                {
+                    foodList.map((item) => (
+                        <RestaurantCard key={item.idMeal} foodList={item} />
+                    ))
+                }
             </div>
         </div>
     )
 }
 // RootLayout
-const App = ()=>{
+const App = () => {
     return (
         <>
-            <Header/>
-            <Body/>
+            <Header />
+            <Body />
         </>
     )
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
-root.render(<App/>)
+root.render(<App />)
